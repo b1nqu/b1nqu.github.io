872828a696bb6e064820087bf0e93dddc8e7ebbf
binqu <bpatrykqwerty@gmail.com>
2024-02-03 18:34:22 +0100

    update
    ..

--- hamburgerbutton.js
+++ hamburgerbutton.js
@@ -1,26 +1,20 @@
-const topnav = document.querySelector('#topnav');
+const topnavmobile = document.querySelector('#topnav-mobile');
 const hamburger = document.querySelector('#hamburger');
 
 // On page load
 
-function handleResize() {
-  if (window.innerWidth <= 700) {
-    topnav.classList.remove('show'); // hide menu if mobile
-  } else {
-    topnav.classList.add('show'); // show menu if desktop
-  }
-}
+function handleResize() {}
 
 window.addEventListener('load', handleResize);
 window.addEventListener('resize', handleResize); 
 
 hamburger.addEventListener('click', () => {
-  topnav.classList.toggle('show'); 
+  topnavmobile.classList.toggle('show'); 
 });
 
 // Close on resize
 window.addEventListener('resize', () => {
   if (window.innerWidth > 700) {
-    topnav.classList.remove('show');
+    topnavmobile.classList.remove('show');
   }
 });
